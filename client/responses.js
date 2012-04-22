3896e7e2ab43a01ca68a2731c6b54379ce1d52d2
Matthew Zeman <zeman.matt@gmail.com>
2012-04-22 02:49:24 -0500

    Simple Add/Remove via webinterface

--- client/responses.js
+++ client/responses.js
@@ -7,11 +7,34 @@ Template.responses.responses = function () {
   return Responses.find({}, {sort: {trigger: 1}});
 };
 
-/*
-Template.response.trigger = function () {
-  return this.trigger;
+Template.response.selected = function () {
+  return Session.equals('selected_response', this._id) ? 'selected_response' : '';
 };
-Template.response.phrase = function () {
-  return this.phrase;
+
+Template.response.events = {
+  'click': function (evt) {
+    Session.set('selected_response', this._id);
+  }
 };
-*/
+
+Template.delete_response.events = {
+  'mousedown': function (evt) {
+    requestID = Session.get('selected_response');
+    if(requestID != null){
+    	Responses.remove(requestID);
+    	Session.set('select_response', null);
+    }
+  }
+};
+
+Template.add_response.events = {
+  'click .addResponse': function (evt) { 
+  	if ($("input#trigger").val() != "" && $("input#phrase").val()){
+  	  var timestamp = (new Date()).getTime();
+      var response_id = Responses.insert({trigger: $("input#trigger").val(), phrase: $("input#phrase").val(), timestamp: timestamp});
+      $("input").val("");
+  	}else{
+  		alert("Please input a trigger and a phrase");
+  	}
+  }
+};
